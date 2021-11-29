b5f475b2c49ec4b4a0c58d19b6d520810a04819d
Leonid Chirkov <leonidchirkovul@gmail.com>
2021-11-29 20:21:29 +0400

    All project rubocop

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,5 +1,5 @@
 class Api::ApplicationController < ApplicationController
-    include AuthHelper
-    helper_method :current_user
-    respond_to :json
+  include AuthHelper
+  helper_method :current_user
+  respond_to :json
 end
